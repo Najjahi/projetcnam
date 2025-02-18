3ed336be8cc19936786ec1866d68a1db1ceee4d3
imane <i.najjihi@hotmail.com>
2025-02-18 16:38:19 +0100

    any

--- projet/__init__.py
+++ projet/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+<<<<<<< HEAD
 from flask_login import LoginManager
 
 app = Flask(__name__)
@@ -11,9 +12,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///imasouk.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+=======
+app = Flask(__name__)
+from flask_login import LoginManager
+app.config[ "SECRET_KEY" ] = "62913a7dac3933f87a84626fcdeaaf9e2653f0a000843efd9bf2b31ba4767402"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///imasouk.db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
+
+>>>>>>> f62f24c (Premier commit avec les fichiers)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
+<<<<<<< HEAD
 
+=======
+>>>>>>> f62f24c (Premier commit avec les fichiers)
 from projet import routes
--- projet/forms.py
+++ projet/forms.py
@@ -1,8 +1,13 @@
 from tokenize import String
 from flask_wtf import FlaskForm
+<<<<<<< HEAD
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+=======
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from projet.models import User
+>>>>>>> f62f24c (Premier commit avec les fichiers)
 from wtforms.validators import (
     DataRequired,
     Length,
@@ -14,7 +19,10 @@ from wtforms.validators import (
 
 from projet.models import User
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> f62f24c (Premier commit avec les fichiers)
 class RegistrationForm(FlaskForm):
     fname = StringField(
         "First Name", validators=[DataRequired(), Length(min=2, max=25)]
@@ -34,13 +42,20 @@ class RegistrationForm(FlaskForm):
         ],
     )
     confirm_password = PasswordField(
+<<<<<<< HEAD
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("S'inscrire")
+=======
+        "Confirmation du mot de passe", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("S'enregistrer")
+>>>>>>> f62f24c (Premier commit avec les fichiers)
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
+<<<<<<< HEAD
             raise ValidationError(
                 "Username already exists! Please chosse a different one"
             )
@@ -51,6 +66,13 @@ class RegistrationForm(FlaskForm):
             raise ValidationError("Email already exists! Please chosse a different one")
 
 
+=======
+            raise ValidationError("Ce nom d'utilisateur est déjà pris. Veuillez en choisir un autre.")
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Cet email est déjà pris. Veuillez en choisir un autre.")
+>>>>>>> f62f24c (Premier commit avec les fichiers)
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField(
@@ -59,6 +81,7 @@ class LoginForm(FlaskForm):
             DataRequired(),
         ],
     )
+<<<<<<< HEAD
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log In")
 
@@ -88,3 +111,7 @@ class UpdateProfileForm(FlaskForm):
                 raise ValidationError(
                     "Email already exists! Please chosse a different one"
                 )
+=======
+    remember = BooleanField("Souviens-toi de moi")
+    submit = SubmitField("Se connecter")
+>>>>>>> f62f24c (Premier commit avec les fichiers)
